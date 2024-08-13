c8a532c53a7ee6ea90e28bb1bdea5d22e3c8379d
KavyaGJS <kavyamujk@gmail.com>
2024-08-13 13:35:46 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nlppack',  
+    version='0.0.1',  
+    description='A Python package for NLP tasks and utilities',
+    url='https://github.com/KavyaGJS/nlppack',  
+    author='KavyaGJS', 
+    author_email='kavyamujk@gmail.com',  
+    packages=['nlppack'],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
